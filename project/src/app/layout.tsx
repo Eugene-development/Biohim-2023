bcf9cddd2c3e531a728edc664c17d305d39a4973
Eugene Che <indexpro24@gmail.com>
2023-08-11 16:50:10 +0300

    c

--- project/src/app/layout.tsx
+++ project/src/app/layout.tsx
@@ -16,6 +16,52 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        {/* <link rel="icon" href="../logo/favicon.ico" sizes="any" /> */}
+        <script src="//code.jivo.ru/widget/yZuDqsMQDU" async></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+                        (function (m, e, t, r, i, k, a) {
+                          m[i] =
+                            m[i] ||
+                            function () {
+                              (m[i].a = m[i].a || []).push(arguments);
+                            };
+                          var z = null;
+                          m[i].l = 1 * new Date();
+                          for (var j = 0; j < document.scripts.length; j++) {
+                            if (document.scripts[j].src === r) {
+                              return;
+                            }
+                          }
+                          (k = e.createElement(t)),
+                            (a = e.getElementsByTagName(t)[0]),
+                            (k.async = 1),
+                            (k.src = r),
+                            a.parentNode.insertBefore(k, a);
+                        })(window, document, 'script', 'https://mc.yandex.ru/metrika/tag.js', 'ym');
+
+                        ym(90256919, 'init', {
+                          clickmap: true,
+                          trackLinks: true,
+                          accurateTrackBounce: true,
+                          webvisor: true
+                        })
+                    `,
+          }}
+        />
+        <noscript>
+          <div>
+            <img
+              src="https://mc.yandex.ru/watch/90256919"
+              style={{ position: "absolute", left: "-9999px" }}
+              alt=""
+            />
+          </div>
+        </noscript>
+      </head>
+
       <body className={inter.className}>
         <Info_v1 />
         <Menu_v1 />
