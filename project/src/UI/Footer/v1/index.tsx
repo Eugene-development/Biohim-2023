543ab27f887b542f8d1e458cae4dce490c3c679d
Eugene Che <indexpro24@gmail.com>
2023-08-19 16:11:32 +0300

    с

--- project/src/UI/Footer/v1/index.tsx
+++ project/src/UI/Footer/v1/index.tsx
@@ -1,3 +1,69 @@
+import Link from "next/link"
 export default () => {
-  return <div>Footer</div>
+  return (
+    <footer className="bg-indigo-950">
+  <div className="p-4 py-6 mx-auto max-w-screen-xl md:p-8 lg:p-10">
+      <div className="gap-8 space-y-12 sm:grid sm:grid-cols-2 lg:grid-cols-3 sm:space-y-0">
+          <address className="not-italic">
+              <p className="mb-6 text-sm font-semibold text-gray-50 uppercase dark:text-white">New York</p>
+              <p className="mb-3 text-gray-50 dark:text-gray-400">Huntersville,<br/> 957 Hill Hills Suite 491, United States
+              </p>
+              <ul className="space-y-3 text-gray-50 dark:text-gray-400">
+                  <li className="flex items-center">
+                      <svg className="mr-2 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M7 2a2 2 0 00-2 2v12a2 2 0 002 2h6a2 2 0 002-2V4a2 2 0 00-2-2H7zm3 14a1 1 0 100-2 1 1 0 000 2z" clip-rule="evenodd"></path></svg>
+                      Office: +12(3) 456 7890 1234
+                  </li>
+                  <li className="flex items-center">
+                      <svg className="mr-2 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z"></path><path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z"></path></svg>
+                      Support: <a href="mailto:company@name.com" className="ml-2 hover:underline">company@name.com</a>
+                  </li>
+              </ul>
+          </address>
+          <address className="not-italic">
+              <p className="mb-6 text-sm font-semibold text-gray-50 uppercase dark:text-white">Rome</p>
+              <p className="mb-3 text-gray-50 dark:text-gray-400">Piazza di Spagna,<br/> 00187 Roma RM, Italy
+              </p>
+              <ul className="space-y-3 text-gray-50 dark:text-gray-400">
+                  <li className="flex items-center">
+                      <svg className="mr-2 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M7 2a2 2 0 00-2 2v12a2 2 0 002 2h6a2 2 0 002-2V4a2 2 0 00-2-2H7zm3 14a1 1 0 100-2 1 1 0 000 2z" clip-rule="evenodd"></path></svg>
+                      Office: +12(3) 456 7890 1234
+                  </li>
+                  <li className="flex items-center">
+                      <svg className="mr-2 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z"></path><path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z"></path></svg>
+                      Support: <a href="mailto:company@name.com" className="ml-2 hover:underline">company@name.it</a>
+                  </li>
+              </ul>
+          </address>
+          <address className="not-italic">
+              <p className="mb-6 text-sm font-semibold text-gray-50 uppercase dark:text-white">London</p>
+              <p className="mb-3 text-gray-50 dark:text-gray-400">Fulham Rd,<br/>London SW6 1HS, United Kingdom
+              </p>
+              <ul className="space-y-3 text-gray-50 dark:text-gray-400">
+                  <li className="flex items-center">
+                      <svg className="mr-2 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M7 2a2 2 0 00-2 2v12a2 2 0 002 2h6a2 2 0 002-2V4a2 2 0 00-2-2H7zm3 14a1 1 0 100-2 1 1 0 000 2z" clip-rule="evenodd"></path></svg>
+                      Office: +12(3) 456 7890 1234
+                  </li>
+                  <li className="flex items-center">
+                      <svg className="mr-2 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z"></path><path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z"></path></svg>
+                      Support: <a href="mailto:company@name.com" className="ml-2 hover:underline">company@name.co.uk</a>
+                  </li>
+              </ul>
+          </address>
+      </div>
+      <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8"/>
+      <div className="flex flex-col justify-between items-center lg:flex-row">
+          <Link href="/" className="flex items-center mb-6 text-2xl font-semibold text-gray-50 lg:mb-0 dark:text-white">
+              <img
+              className="h-8 mr-3 w-auto"
+              src="https://storage.yandexcloud.net/brand-logo/biohim/logo/logo.svg"
+              alt="логотип"
+            />
+              Биохим  
+          </Link>
+          <span className="block mb-4 text-sm text-gray-50 dark:text-gray-400 lg:mb-0">© 2021-2023 <Link href="/" className="hover:underline">Биохим™</Link>. Все права защищены.
+          </span>
+      </div>
+  </div>
+</footer>
+  )
 }
--- project/src/UI/Stats/v1/index.tsx
+++ project/src/UI/Stats/v1/index.tsx
@@ -10,7 +10,7 @@ export default () => {
   return (
     <div className="relative bg-white">
       <div className="mx-auto max-w-full lg:grid lg:grid-cols-12 lg:gap-x-8 lg:px-16">
-        <div className="px-6 pb-24 pt-10 sm:pb-32 lg:col-span-7 lg:px-0 lg:pb-56 lg:pt-24 xl:col-span-6">
+        <div className="z-50 px-6 pb-24 pt-10 sm:pb-32 lg:col-span-7 lg:px-0 lg:pb-56 lg:pt-24 xl:col-span-6">
           <div className="mx-auto max-w-2xl lg:mx-0">
             
             <h1 className="mt-24 text-4xl font-bold tracking-tight text-indigo-950 sm:mt-10 sm:text-6xl">
@@ -28,32 +28,44 @@ export default () => {
               Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet
               fugiat veniam occaecat fugiat aliqua.
             </p>
+            <p className="mt-6 text-lg leading-8 text-gray-600">
+              Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet
+              fugiat veniam occaecat fugiat aliqua.
+            </p>
             <div className="mt-10 flex items-center gap-x-6">
               <Link
                 href="/about"
-                className="rounded-md bg-yellow-500 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                className="rounded-full bg-lime-500 px-6 py-3 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
               >
                 Читать подробнее
               </Link>
             </div>
           </div>
 
-          <dl className="mt-16 grid grid-cols-1 gap-0.5 overflow-hidden rounded-2xl text-center sm:grid-cols-2 lg:grid-cols-3">
+          <dl className="mt-16 -mr-32 grid grid-cols-1 gap-0.5 overflow-hidden rounded-2xl text-center sm:grid-cols-2 lg:grid-cols-3">
             {stats.map((stat) => (
-              <div key={stat.id} className="flex flex-col bg-indigo-950 p-8">
-                <dt className="text-sm font-semibold leading-6 text-gray-100">{stat.name}</dt>
-                <dd className="order-first text-3xl font-semibold tracking-tight text-gray-50">{stat.value}</dd>
+              <div key={stat.id} className=" flex flex-col bg-indigo-950 p-8">
+              <dd className=" order-first text-4xl font-semibold tracking-tight text-lime-200">{stat.value}</dd>
+
+                <dt className="mt-4 text-base font-semibold leading-6 text-gray-100">{stat.name}</dt>
               </div>
             ))}
           </dl>
         </div>
-        <div className="relative lg:col-span-5 lg:-mr-8 xl:absolute xl:inset-0 xl:left-1/2 xl:mr-0">
+        <div className="z-40 relative lg:col-span-5 xl:absolute xl:inset-0 xl:left-1/2 xl:mr-0">
           <img
-            className="aspect-[3/2] w-full bg-gray-50 object-cover lg:absolute lg:inset-0 lg:aspect-auto lg:h-full"
-            src="https://images.unsplash.com/photo-1498758536662-35b82cd15e29?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2102&q=80"
-            alt=""
-          />
+              src="https://mos-522755.oml.ru/thumb/2/qCRqK04zbRqzb5JumjWnOA/1920r1080/d/55291941_2.jpg"
+              alt=""
+              className="mt-8 h-96 w-full max-w-lg rounded-2xl object-cover sm:mt-16 lg:mt-0 lg:max-w-3xl xl:row-span-2 xl:row-end-2 xl:mt-16"
+            />
+          <img
+              src="https://mos-522755.oml.ru/thumb/2/tN3tnp0BE1RhibfOSUDzJA/1920r1080/d/55291937_2.jpg"
+              alt=""
+              className="mt-8 h-96 w-full max-w-lg rounded-2xl object-cover sm:mt-16 lg:mt-0 lg:max-w-3xl xl:row-span-2 xl:row-end-2 xl:mt-16"
+            />
         </div>
+        
+
       </div>
     </div>
   )
